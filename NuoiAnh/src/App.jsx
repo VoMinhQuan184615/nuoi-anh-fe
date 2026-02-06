5d0249789399146def9c5fdc867f98e33fe71106
VoMinhQuan184615 <vominhqun14112004@gmail.com>
2026-02-07 04:17:29 +0700

    feat: login form ui

--- NuoiAnh/src/App.jsx
+++ NuoiAnh/src/App.jsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./features/dashboard/pages/Dashboard";
+import LoginPage from "./features/login/pages/LoginPage";
 import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
-    <>
-      <Dashboard />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
       <Toaster />
-    </>
+    </BrowserRouter>
   );
 }
 
